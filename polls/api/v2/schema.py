15881ae0129dd5711c06a72857a3975cebd934a3
Bestin Mathews James <bestinmathewsjames@Bestins-MacBook-Pro.local>
2021-05-06 10:56:15 +0530

    added Choice model in Question -API

--- polls/api/v2/schema.py
+++ polls/api/v2/schema.py
@@ -1,18 +1,27 @@
 import graphene
 from graphene_django import DjangoObjectType
-from polls.models import Question
+from polls.models import Question, Choice
+
+
+class ChoiceType(DjangoObjectType):
+    class Meta:
+        model = Choice
+        fields = ['choice_text', 'votes']
 
 
 class QuestionType(DjangoObjectType):
+    choice = ChoiceType()
+
     class Meta:
         model = Question
-        fields = '__all__'
+        fields = ['id', 'question_text', 'expiry_date', 'pub_date', 'added_by',
+                  'choice']
 
 
 class Query(graphene.ObjectType):
     all_questions = graphene.List(QuestionType)
 
-    def resolve_all_questions(self,info):
+    def resolve_all_questions(self, info):
         return Question.objects.all()
 
 
